069573a21a520d4227ba48bd167d0423f775d05e
Esteban <cepalacios@outlook.com>
2022-12-21 08:33:24 -0500

    create a circle inside the map

--- app/src/main/java/com/notylines/elbus/ui/screens/run/RunScreen.kt
+++ app/src/main/java/com/notylines/elbus/ui/screens/run/RunScreen.kt
@@ -1,18 +1,18 @@
 package com.notylines.elbus.ui.screens.run
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.Circle
 import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.Marker
-import com.google.maps.android.compose.MarkerState
 import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
@@ -22,24 +22,30 @@ fun RunScreen(navController: NavController) {
 
         ) {
 
-        val singapore = LatLng(1.35, 103.87)
+        val casa = LatLng(-0.1964991,-78.5082015)
         val cameraPositionState = rememberCameraPositionState {
-            position = CameraPosition.fromLatLngZoom(singapore, 10f)
+            position = CameraPosition.fromLatLngZoom(casa, 19f)
         }
 
         Column(
             modifier = Modifier.fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            GoogleMap(
-                modifier = Modifier.fillMaxWidth(),
-                cameraPositionState = cameraPositionState
-            ) {
-                Marker(
-                    state = MarkerState(position = singapore),
-                    title = "Singapore",
-                    snippet = "Marker in Singapore"
-                )
+            Box(modifier = Modifier.padding(12.dp)) {
+
+                GoogleMap(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(520.dp),
+                    cameraPositionState = cameraPositionState
+                ) {
+                    Circle(
+                        center = casa,
+                        fillColor = MaterialTheme.colors.secondary,
+                        radius = 2.0,
+                        strokeColor = Color.Transparent
+                    )
+                }
             }
         }
     }
