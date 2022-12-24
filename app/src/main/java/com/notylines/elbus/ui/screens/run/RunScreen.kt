674d8b88377f7ff19c59ea6c9d39b3c337d9de7f
Esteban <cepalacios@outlook.com>
2022-12-24 08:04:33 -0500

    mend

--- app/src/main/java/com/notylines/elbus/ui/screens/run/RunScreen.kt
+++ app/src/main/java/com/notylines/elbus/ui/screens/run/RunScreen.kt
@@ -1,21 +1,10 @@
 package com.notylines.elbus.ui.screens.run
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.google.android.gms.maps.model.CameraPosition
-import com.google.android.gms.maps.model.LatLng
-import com.google.maps.android.compose.Circle
-import com.google.maps.android.compose.GoogleMap
-import com.google.maps.android.compose.MapUiSettings
-import com.google.maps.android.compose.rememberCameraPositionState
 
 @Composable
 fun RunScreen(navController: NavController) {
